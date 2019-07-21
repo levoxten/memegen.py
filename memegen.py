e4333f0953776faf979f8342f9375f9588b0138e
levoxten <52978274+levoxten@users.noreply.github.com>
2019-07-21 18:18:47 -0400

    added more memes
    added more memesadded more memesadded more memesadded more memesadded more memesadded more memesadded more memesadded more memes

--- memegen.py
+++ memegen.py
@@ -2,7 +2,7 @@ import random
 
 # Meme Gen - WOLQC 2019 (c)
 
-memes = ["'Levoxten is a meme' - Levoxten, 2019", 'Oh god please why', 'Just say YES!', 'PETA is a joke', 'End this', 'Doge', 'Dogecoin Cryptocurrency', "'Optic's discord profile picture is a meme' - OpticLight (optislight on github), 2019"]
+memes = ["'Levoxten is a meme' - Levoxten, 2019", 'Oh god please why', 'Just say YES!', 'PETA is a joke', 'End this', 'Doge', 'Dogecoin Cryptocurrency', "'Optic's discord profile picture is a meme' - OpticLight (optislight on github), 2019", '" i wish people would stop..' - made by minecraftcito, (aka levoxten)"]
 
 while 1:
     randomvar = input('Type stuff (or not) then press the "enter" key > ')
